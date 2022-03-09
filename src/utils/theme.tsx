e4f3f9bb62c0bd111490fdc1a2bd8c302125f67d
satoshi <3104nit@gmail.com>
2022-03-09 13:10:32 +0900

    added default breakepoints

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -1,12 +1,22 @@
 import { extendTheme } from '@chakra-ui/react'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 const fonts = { mono: `'Menlo', monospace` }
+
+const breakpoints = createBreakpoints({
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+  '2xl': '96em',
+})
 
 const theme = extendTheme({
   colors: {
     black: '#16161D',
     darkBlue: '#002D59',
   },
+  breakpoints,
   fonts,
 })
 
