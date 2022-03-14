5c1d904bbf895d0e88c0f16b874e311b85322fee
satoshi <3104nit@gmail.com>
2022-03-14 22:03:15 +0900

    fix IconButton component

---
+++ src/components/atoms/buttons/IconButton.tsx
@@ -0,0 +1,20 @@
+import { Button, ButtonProps, IconProps } from '@chakra-ui/react'
+
+interface Props {
+  onClick: () => void
+  children: IconProps
+}
+
+export const IconButton = ({ onClick, children, ...rest }: Props) => {
+  return (
+    <Button
+      variant="ghost"
+      color="white"
+      _hover={{ bgColor: 'blackAlpha.400' }}
+      {...rest}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  )
+}
