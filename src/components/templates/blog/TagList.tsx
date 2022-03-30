feaa685edf6b0534d766741c13f328031ad73bab
satoshi <3104nit@gmail.com>
2022-03-30 12:35:46 +0900

    fix props

--- src/components/templates/blog/TagList.tsx
+++ src/components/templates/blog/TagList.tsx
@@ -4,35 +4,18 @@ import { ChakraLink } from '../../atoms/ChakraLink'
 import { TagButton } from '../../atoms/buttons/TagButton'
 import { Tag } from '../../../types/main'
 
-export const TagList = () => {
-  const tags: Tag[] = [
-    {
-      _id: '1',
-      title: 'Python',
-      slug: 'python',
-      posts: [],
-    },
-    {
-      _id: '2',
-      title: 'Next.js',
-      slug: 'nextjs',
-      posts: [],
-    },
-    {
-      _id: '3',
-      title: 'Django REST Framework',
-      slug: 'django-rest-framework',
-      posts: [],
-    },
-  ]
+type Props = {
+  tags: Tag[]
+}
 
+export const TagList = ({ tags }: Props) => {
   return (
     <Stack direction="column" alignItems="baseline">
       <Heading>Tags</Heading>
 
       <Flex direction="row" wrap="wrap" gap="2">
         {tags?.map((tag) => (
-          <Link key={tag._id} href={`/posts/tags/${tag.slug}`} passHref>
+          <Link key={tag.id} href={`/posts/tags/${tag.slug}`} passHref>
             <ChakraLink>
               <TagButton title={tag.title} />
             </ChakraLink>
