76cb6f86f47697a5d229663ffec73231f22a0878
satoshi <3104nit@gmail.com>
2022-03-31 21:26:03 +0900

    added react-query hydrate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,33 @@ import { AppProps } from 'next/app'
 
 import { RecoilRoot } from 'recoil'
 
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { useState } from 'react'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  )
   return (
-    <RecoilRoot>
-      <ChakraProvider resetCSS theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <ChakraProvider resetCSS theme={theme}>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </RecoilRoot>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
