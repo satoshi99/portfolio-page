255ef7ac84e3af4df1e7ef72f7554b1f877a4eed
satoshi <3104nit@gmail.com>
2022-04-18 00:44:30 +0900

    add recoil atoms

--- src/lib/recoil-atoms.ts
+++ src/lib/recoil-atoms.ts
@@ -1,6 +1,28 @@
 import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
+import { UpdatePostInputs } from '../types/post'
+
+const { persistAtom } = recoilPersist()
 
 export const csrfState = atom<boolean>({
   key: 'csrfState',
   default: false,
+  effects_UNSTABLE: [persistAtom],
+})
+
+export const updatePostState = atom<UpdatePostInputs>({
+  key: 'updatePost',
+  default: {
+    data: {
+      id: '',
+      thumbnail: '',
+      title: '',
+      description: '',
+      content: '',
+      url_slug: '',
+      is_public: false,
+    },
+    tags: [],
+  },
+  effects_UNSTABLE: [persistAtom],
 })
