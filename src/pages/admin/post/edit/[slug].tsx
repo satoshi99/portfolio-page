2be2832e8a2f512650c5b3f3cf4f126e00843476
satoshi <3104nit@gmail.com>
2022-04-25 13:18:49 +0900

    fix edit post page

--- src/pages/admin/post/edit/[slug].tsx
+++ src/pages/admin/post/edit/[slug].tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
 import { EditPostTemplate } from '../../../../components/templates/admin/EditPostTemplate'
 import { useQueryTags } from '../../../../hooks/useQueryTags'
 
 const EditPost: NextPage = () => {
-  const router = useRouter()
   const { data: tagsData } = useQueryTags()
 
   return <EditPostTemplate tags={tagsData} />
